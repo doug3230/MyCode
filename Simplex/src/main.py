7dd23a1b92e02a52a2d276060308d3e45ed1f548
rdouglas <doug3230@mylaurier.ca>
2014-01-17 22:11:20 -0500

    Created Template for main program

---
+++ Simplex/src/main.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+Simplex Main
+[description of main program]
+-------------------------------------------------------
+Author:  Richard Douglas
+Email:   doug3230@mylaurier.ca
+Version: 2014-01-17
+-------------------------------------------------------
+"""
+import functions
+
+#[constants]
+
+#[rest of program code]
