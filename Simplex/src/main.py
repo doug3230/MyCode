ceef53b3f2e4dc3c38fbdf85b1d4c8f6d3b5bc7d
rdouglas <doug3230@mylaurier.ca>
2014-01-18 16:12:26 -0500

    Tested new methods

--- Simplex/src/main.py
+++ Simplex/src/main.py
@@ -10,5 +10,8 @@ Version: 2014-01-17
 """
 from matrix import Matrix
 
-my_matrix = Matrix([[1,2,3], [10,100,1000],[100000000000,9001,114]])
-print(my_matrix)
+my_matrix = Matrix([[1,2], [3,4]])
+my_other_matrix = Matrix([[0,5],[6,7]])
+
+print(my_matrix.add(my_other_matrix))
+print(my_matrix.subtract(my_other_matrix))
