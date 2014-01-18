49a1cc6ddbdd365ce91ac8d8b079e843e275127b
rdouglas <doug3230@mylaurier.ca>
2014-01-17 22:09:38 -0500

    Created Template for functions library

---
+++ Simplex/src/functions.py
@@ -0,0 +1,26 @@
+"""
+-------------------------------------------------------
+Simplex Functions
+[description of functions]
+-------------------------------------------------------
+Author:  Richard Douglas
+Email:   doug3230@mylaurier.ca
+Version: 2014-01-17
+-------------------------------------------------------
+"""
+#[import statements]
+
+#[constants]
+
+# def [function definition]:
+#     """
+#     -------------------------------------------------------
+#     [function description]
+#     -------------------------------------------------------
+#     Preconditions:
+#        [parameter name - parameter description (parameter type and constraints)]
+#     Postconditions:
+#        [returns: or prints:]
+#        [return value name - return value description (return value type)] 
+#     -------------------------------------------------------
+#     """
