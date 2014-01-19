9dd9c14a40612b46fa753c76c1eed01e9eb42319
rdouglas <doug3230@mylaurier.ca>
2014-01-18 21:49:20 -0500

    started work on the main program

--- Simplex/src/functions.py
+++ Simplex/src/functions.py
@@ -8,19 +8,76 @@ Email:   doug3230@mylaurier.ca
 Version: 2014-01-17
 -------------------------------------------------------
 """
-#[import statements]
+from matrix import Matrix
 
-#[constants]
+COMMANDS = (("Clear", "Clears all matrices from memory.")
+            ("Help", "Display commands and descriptions."), 
+            ("Load", "Load a new matrix."),
+            ("Pivot", "Perform a pivot on the matrix."),
+            ("Quit", "Quit program."),
+            ("Undo", "Change to previous matrix."))
 
-#def [function definition]:
-#    """
-#    -------------------------------------------------------
-#    [function description]
-#    -------------------------------------------------------
-#    Preconditions:
-#       [parameter name - parameter description (parameter type and constraints)]
-#    Postconditions:
-#       [returns: or prints:]
-#       [return value name - return value description (return value type)] 
-#    -------------------------------------------------------
-#    """
+def print_help():
+    for command in COMMANDS:
+        print("{0}: {1}".format(command[0], command[1]))
+    return
+
+def prompt_for_size():
+    """
+    -------------------------------------------------------
+    [function description]
+    -------------------------------------------------------
+    Preconditions:
+       [parameter name - parameter description (parameter type and constraints)]
+    Postconditions:
+       [returns: or prints:]
+       [return value name - return value description (return value type)] 
+    -------------------------------------------------------
+    """ 
+    rows = int(input("Enter the number of rows: "))
+    cols = int(input("Enter the number of columns: "))
+    return (rows, cols)
+
+def prompt_for_location():
+    """
+    -------------------------------------------------------
+    [function description]
+    -------------------------------------------------------
+    Preconditions:
+       [parameter name - parameter description (parameter type and constraints)]
+    Postconditions:
+       [returns: or prints:]
+       [return value name - return value description (return value type)] 
+    -------------------------------------------------------
+    """
+    row_index = int(input("Enter the row index: "))
+    col_index = int(input("Enter the column index: "))
+    return (row_index, col_index) 
+
+def prompt_for_matrix(rows, cols):
+    """
+    -------------------------------------------------------
+    [function description]
+    -------------------------------------------------------
+    Preconditions:
+       [parameter name - parameter description (parameter type and constraints)]
+    Postconditions:
+       [returns: or prints:]
+       [return value name - return value description (return value type)] 
+    -------------------------------------------------------
+    """
+    values = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            entry = input("Enter entry ({0}, {1}): ".format(i, j))
+            if entry.startswith("-") and entry[1:].isdigit():
+                entry = int(entry)
+            elif entry.isdigit():
+                entry = int(entry)
+            else:
+                entry = float(entry)
+            row.append(entry)
+        values.append(row)
+    return Matrix(values)
+    
--- Simplex/src/main.py
+++ Simplex/src/main.py
@@ -8,12 +8,37 @@ Email:   doug3230@mylaurier.ca
 Version: 2014-01-17
 -------------------------------------------------------
 """
-from matrix import Matrix
+import functions
 
-my_matrix = Matrix([[1, 2], [3, 4]])
-print(my_matrix.rows(), my_matrix.cols())
-for row in my_matrix._data:
-    print(row)
-print(my_matrix.pivot(0, 1))
+PROMPT = ">> "
+matrix_states = []
 
-# print(my_matrix.pivot(0, 1))
+print("Welcome to the Simplex program.")
+print('Enter "Help" for a list of commands.')
+command = input(PROMPT).lower()
+while command != "quit":
+    try:
+        if command == "help":
+            functions.print_help()
+        elif command == "clear":
+            matrix_states = []
+        elif command == "load":
+            rows, cols = functions.prompt_for_size()
+            matrix = functions.prompt_for_matrix(rows, cols)
+            matrix_states.append(matrix)
+        elif command == "pivot":
+            r, c = functions.prompt_for_location()
+            cur_matrix = matrix_states[-1]
+            pivoted_matrix = cur_matrix.pivot(r, c)
+            matrix_states.append(pivoted_matrix)
+        elif command == "undo":
+            if len(matrix_states) > 0:
+                matrix_states.pop()
+        else:
+            print('Invalid command, enter "Help" for a list of commands.')
+    except Exception as inst:
+        print("Error: {0}".format(inst))
+        
+    if len(matrix_states) > 0:
+        print(matrix_states[-1])
+    command = input(PROMPT).lower()
