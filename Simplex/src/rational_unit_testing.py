9c1f1ee0588907c174f2a3fd1770e824db255208
rdouglas <doug3230@mylaurier.ca>
2014-01-23 04:14:02 -0500

    minor change

--- Simplex/src/rational_unit_testing.py
+++ Simplex/src/rational_unit_testing.py
@@ -18,7 +18,7 @@ class RatTest(unittest.TestCase):
         pass
 
     def testConstructor(self):
-        self.assertEqual(Rational(1,2), Rational(2,4), "Terms not reduced by gcd properly")
+        self.assertEqual(Rational(121,132), Rational(11,12), "Terms not reduced by gcd properly")
         self.assertEqual(Rational(0, 5), Rational(0,2), "0s not standardized")
         self.assertEqual(Rational(-9001,666), Rational(9001, -666), "Negatives not working")
         pass
