8f1adde8e03b936cb05141021ed99bf25958ae9a
rdouglas <doug3230@mylaurier.ca>
2014-01-25 21:56:49 -0500

    created constructor and getters/setters

--- Solitaire/src/card/Card.java
+++ Solitaire/src/card/Card.java
@@ -2,34 +2,65 @@ package card;
 
 /**
  * This class models a playing card used in games like Solitaire and Black Jack.
- * </br>
- * It has two attributes:
+ * </br> It has two attributes:
  * <ul>
  * <li><b>rank</b> (the numerical value of the card)</li>
  * <li><b>suit</b> (which family the card belongs to)</li>
  * </ul>
- * Thus the two of clubs has rank 2, and suit "clubs".
- * Aces have rank 1. Jacks Queens, and Kings have ranks 
- * 11, 12, and 13 respectively.
- * </br>
+ * Thus the two of clubs has rank 2, and suit "clubs". Aces have rank 1. Jacks
+ * Queens, and Kings have ranks 11, 12, and 13 respectively. </br>
  * 
  * @author Richard Douglas
  */
 
 public class Card {
-	
-	//Constants
-	//---------
+
+	// Constants
+	// ---------
 	public static final int MIN_RANK = 1;
 	public static final int MAX_RANK = 13;
 	public static final String CLUBS = "Clubs";
 	public static final String DIAMONDS = "Diamonds";
 	public static final String HEARTS = "Hearts";
 	public static final String SPADES = "Spades";
-	public static final String[] SUITS = {CLUBS, DIAMONDS, HEARTS, SPADES};
-	
-	//Data Members
-	//------------
+	public static final String[] SUITS = { CLUBS, DIAMONDS, HEARTS, SPADES };
+
+	// Data Members
+	// ------------
 	private int rank;
 	private String suit;
+
+	// Constructors
+	// ------------
+	public Card(int rank, String suit) {
+		setRank(rank);
+		setSuit(suit);
+	}
+
+	// Methods
+	// -------
+
+	public int getRank() {
+		return rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setRank(int rank) {
+		if (rank < MIN_RANK && MAX_RANK < rank)
+			throw new RuntimeException("Card rank must be between " + MIN_RANK
+					+ " and " + MAX_RANK + " (inclusive)");
+		this.rank = rank;
+	}
+
+	public void setSuit(String suit) {
+		int i = 0;
+		while (i < SUITS.length && !suit.equalsIgnoreCase(SUITS[i]))
+			i += 1;
+		if (i == SUITS.length)
+			throw new RuntimeException("Card suit must be in " + SUITS);
+		this.suit = SUITS[i];
+	}
 }
