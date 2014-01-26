b29aab8b06ce752448111a196310c095d59a7f59
rdouglas <doug3230@mylaurier.ca>
2014-01-25 21:23:28 -0500

    started writing javadoc for Card class

--- Solitaire/src/card/Card.java
+++ Solitaire/src/card/Card.java
@@ -1,5 +1,21 @@
 package card;
 
+/**
+ * This class models a playing card used in games like Solitaire and Black Jack.
+ * </br>
+ * It has two attributes:
+ * <ul>
+ * <li><b>rank</b> (the numerical value of the card)</li>
+ * <li><b>suit</b> (which family the card belongs to)</li>
+ * </ul>
+ * Thus the two of clubs has rank 2, and suit "clubs".
+ * Aces have rank 1. Jacks Queens, and Kings have ranks 
+ * 11, 12, and 13 respectively.
+ * </br>
+ * 
+ * @author Richard Douglas
+ */
+
 public class Card {
 
 }
