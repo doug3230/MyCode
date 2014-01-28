05654ed304f16ea36b6b32575d62c23ea316d901
rdouglas <doug3230@mylaurier.ca>
2014-01-28 14:22:40 -0500

    changed Pile and PileSprite

--- Solitaire/src/pile/Pile.java
+++ Solitaire/src/pile/Pile.java
@@ -1,5 +1,57 @@
 package pile;
 
-public class Pile {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class Pile<E> implements Iterable<E> {
+
+	// Data Members
+	// ------------
+	private boolean isEnforcingRules;
+	private ArrayList<E> elements;
+
+	// Constructors
+	// ------------
+	public Pile() {
+		this.elements = new ArrayList<E>();
+		this.isEnforcingRules = false;
+	}
+
+	// Methods
+	// -------
+	public boolean isEnforcingRules() {
+		return isEnforcingRules;
+	}
+
+	public void enforceRules() {
+		this.isEnforcingRules = true;
+	}
+
+	public void dontEnforceRules() {
+		this.isEnforcingRules = false;
+	}
+
+	public boolean isEmpty() {
+		return elements.isEmpty();
+	}
+
+	public Iterator<E> iterator() {
+		return elements.iterator();
+	}
+
+	public void push(E element) {
+		if (isEnforcingRules && !canPushLegally(element))
+			throw new RuntimeException("Cannot push " + element + " to Pile.");
+		elements.add(0, element);
+	}
+
+	public boolean canPushLegally(E element) {
+		return true;
+	}
+
+	public E draw() {
+		if (elements.isEmpty())
+			throw new RuntimeException("Cannot draw from an empty Pile.");
+		return elements.remove(0);
+	}
 }
---
+++ Solitaire/src/pile/PileSprite.java
@@ -0,0 +1,45 @@
+package pile;
+
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
+import card.CardSprite;
+
+public class PileSprite extends JLabel {
+	
+	//Data Members
+	//------------
+	private Pile<CardSprite> pile;
+	private JLayeredPane layerPane;
+	private int layerIndex;
+	
+	//Constructors
+	//------------
+	public PileSprite(Pile<CardSprite> pile, JLayeredPane layerPane, int layerIndex) {
+		this.pile = pile;
+		this.layerPane = layerPane;
+		this.layerIndex = layerIndex;
+		layerPane.add(this, new Integer(layerIndex));
+	}
+	
+	//Methods
+	//-------
+	public JLayeredPane getLayerPane() {
+		return layerPane;
+	}
+	
+	public Pile<CardSprite> getPile() {
+		return pile;
+	}
+	
+	public int getLayerIndex() {
+		return layerIndex;
+	}
+	
+	public void setLayerIndex(int layerIndex) {
+		this.layerIndex = layerIndex;
+		layerPane.setLayer(this, layerIndex);
+	}
+}
